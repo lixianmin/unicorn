75f36c6a4e33dcea7c2aa716fc51430a3ac9fc95
lixianmin <lixianmin@live.cn>
2022-08-25 20:38:51 +0800

    完善CallbackTools

--- Assets/Code/Unicorn/Common/CallbackTools.cs
+++ Assets/Code/Unicorn/Common/CallbackTools.cs
@@ -13,6 +13,22 @@ namespace Unicorn
 {
     public static class CallbackTools
     {
+        public static void Handle(Action handler, string title)
+        {
+            if (null != handler)
+            {
+                try
+                {
+                    handler();
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine("[CallbackTools.Handle()] {0}, ex= {1},\n\n StackTrace={2}"
+                        , title, ex, ex.StackTrace);
+                }
+            }
+        }
+        
         public static void Handle(ref Action handler, string title)
         {
             if (null != handler)
@@ -35,33 +51,33 @@ namespace Unicorn
             }
         }
 
-        public static void Handle<T>(ref Action<T> handler, T self, string title)
-        {
-            Handle(ref handler, self, title, string.Empty);
-        }
-
-        public static void Handle<T, U>(ref Action<T> handler, T self, string title, U text)
-        {
-            if (null != handler)
-            {
-                try
-                {
-                    //AssertTools.BeginTimeout();
-                    handler(self);
-                    //AssertTools.EndTimeout(Timeout);
-                }
-                catch (Exception ex)
-                {
-                    Console.Error.WriteLine("[CallbackTools.Handle()] {0} {1}, ex= {2},\n\n StackTrace={3},\n\n this= {4}"
-                                            , title, text.ToString(), ex.ToString(), ex.StackTrace, self);
-                }
-                finally
-                {
-                    handler = null; // handler是一个回调，它可能间接引用了大量对象，及时置空;
-                }
-            }
-        }
-
-        //        public static float Timeout = 5f;
+        // public static void Handle<T>(ref Action<T> handler, T self, string title)
+        // {
+        //     Handle(ref handler, self, title, string.Empty);
+        // }
+        //
+        // public static void Handle<T, U>(ref Action<T> handler, T self, string title, U text)
+        // {
+        //     if (null != handler)
+        //     {
+        //         try
+        //         {
+        //             //AssertTools.BeginTimeout();
+        //             handler(self);
+        //             //AssertTools.EndTimeout(Timeout);
+        //         }
+        //         catch (Exception ex)
+        //         {
+        //             Console.Error.WriteLine("[CallbackTools.Handle()] {0} {1}, ex= {2},\n\n StackTrace={3},\n\n this= {4}"
+        //                                     , title, text.ToString(), ex.ToString(), ex.StackTrace, self);
+        //         }
+        //         finally
+        //         {
+        //             handler = null; // handler是一个回调，它可能间接引用了大量对象，及时置空;
+        //         }
+        //     }
+        // }
+        //
+        // //        public static float Timeout = 5f;
     }
 }
