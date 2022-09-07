cf3a1d365c9461d57d109dfb88b01d77cafb1271
lixianmin <lixianmin@live.cn>
2022-09-07 14:50:53 +0800

    refactor

---
+++ Assets/Code/Unicorn/Kit/TestKit.cs
@@ -0,0 +1,39 @@
+//
+//
+// /********************************************************************
+// created:    2022-09-07
+// author:     lixianmin
+//
+// Copyright (C) - All Rights Reserved
+// *********************************************************************/
+// using Unicorn;
+//
+// public class TestKit : KitBase
+// {
+//     
+// }
+//
+// public class PlayerMoveKit : KitBase
+// {
+//
+// }
+//
+// public class HelloKit : KitBase
+// {
+//     
+// }
+//
+// public class WorldKit : KitBase
+// {
+//     
+// }
+//
+// public class FaintKit : KitBase
+// {
+//     
+// }
+//
+// public class FastKit : KitBase
+// {
+//     
+// }
