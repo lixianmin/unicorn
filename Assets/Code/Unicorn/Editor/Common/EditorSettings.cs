aa6cd1c0aa99eebd34d5c3cf71a085aab7a8711b
lixianmin <lixianmin@live.cn>
2022-09-30 10:15:08 +0800

    只有有限的几个平台支持linear空间

--- Assets/Code/Unicorn/Editor/Common/EditorSettings.cs
+++ Assets/Code/Unicorn/Editor/Common/EditorSettings.cs
@@ -18,10 +18,37 @@ namespace Unicorn
         private static void Reload()
         {
             var manifest = UnicornManifest.OpenOrCreate();
-            
-            // 设置colorSpace，默认使用Linear空间
+            _SetColorSpace(manifest);
+        }
+
+        // 设置colorSpace，默认使用Linear空间
+        private static void _SetColorSpace(UnicornManifest manifest)
+        {
             var name = manifest.editorSettings.colorSpace;
             var colorSpace = name == "Linear" ? ColorSpace.Linear : ColorSpace.Gamma;
+            if (colorSpace == ColorSpace.Linear)
+            {
+                // 只有以下平台是支持Linear空间的
+                // 参考：https://docs.unity3d.com/cn/current/Manual/LinearRendering-GammaTextures.html
+                var isValidBuildTarget = false;
+                switch (EditorUserBuildSettings.activeBuildTarget)
+                {
+                    case BuildTarget.StandaloneWindows:
+                    case BuildTarget.StandaloneWindows64:
+                    case BuildTarget.StandaloneOSX:
+                    case BuildTarget.StandaloneLinux64:
+                    case BuildTarget.Android:
+                    case BuildTarget.iOS:
+                        isValidBuildTarget = true;
+                        break;
+                }
+
+                if (!isValidBuildTarget)
+                {
+                    return;
+                }
+            }
+            
             if (PlayerSettings.colorSpace != colorSpace)
             {
                 PlayerSettings.colorSpace = colorSpace;
