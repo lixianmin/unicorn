df1f966bfda7ee4ec9a363fadaba3a5d2dfc89a2
lixianmin <lixianmin@live.cn>
2022-09-30 10:52:17 +0800

    初始调整linear颜色空间导致的sRBGTexture调整

--- Assets/Code/Unicorn/Editor/Common/ImportProcessor/TextureImportProcessor.cs
+++ Assets/Code/Unicorn/Editor/Common/ImportProcessor/TextureImportProcessor.cs
@@ -18,12 +18,26 @@ namespace Unicorn
             var importer = (TextureImporter) assetImporter;
             var isChanged = false;
             
+            // 某些纹理必须位于gamma空间，而另外一些则倾向于是linear空间
+            // https://docs.unity3d.com/2022.2/Documentation/Manual/LinearRendering-LinearTextures.html
+            var mustGammaTexture = importer.textureType is TextureImporterType.Lightmap 
+                or TextureImporterType.DirectionalLightmap 
+                or TextureImporterType.GUI; // GUI这个实指 Editor GUI and Legacy GUI，现在实际用的GUI类型是Sprite那个 
+            
             var isLinear = PlayerSettings.colorSpace == ColorSpace.Linear;
-            if (isLinear && importer.sRGBTexture)
+            if (isLinear)
             {
-                importer.sRGBTexture = false;
-                isChanged = true;
-                Console.WriteLine($"assetPath={assetPath}, (colorSpace=Linear) => (sRGBTexture=false)");
+                if (importer.sRGBTexture && !mustGammaTexture)
+                {
+                    importer.sRGBTexture = false;
+                    isChanged = true;
+                    Console.WriteLine($"assetPath={assetPath}, (colorSpace=Linear) => (sRGBTexture=false)");
+                } else if (!importer.sRGBTexture && mustGammaTexture)
+                {
+                    importer.sRGBTexture = true;
+                    isChanged = true;
+                    Console.WriteLine($"assetPath={assetPath}, (textureType={importer.textureType}) => (sRGBTexture=true)");  
+                }
             }
 
             if (isChanged)
