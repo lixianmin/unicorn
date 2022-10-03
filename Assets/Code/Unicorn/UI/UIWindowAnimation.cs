66a0526d0d697a2041455707ed689da1dd6bebca
lixianmin <lixianmin@live.cn>
2022-10-03 17:42:53 +0800

    使用RunDelayed()代替协程

--- Assets/Code/Unicorn/UI/UIWindowAnimation.cs
+++ Assets/Code/Unicorn/UI/UIWindowAnimation.cs
@@ -23,37 +23,28 @@ namespace Unicorn.UI
             
             _onAnimationDone = onAnimationDone;
             OnAnimationComplete += _OnAnimationDone;
-            CoroutineManager.StartCoroutine(_CoAnimationTimeout(), out _animationRoutine);
+            
+            const float delayedSeconds = 5f;
+            Loom.RunDelayed(_OnAnimationDone, delayedSeconds);
         }
-       
-        private IEnumerator _CoAnimationTimeout()
-        {
-            const float maxTime = 5.0f;
-            var breakTime = Time.time + maxTime;
-            while (Time.time < breakTime)
-            {
-                yield return null;
-            }
 
-            _OnAnimationDone();
-        }
-        
         private void _OnAnimationDone()
         {
-            _animationRoutine?.Kill();
-            
-            _animationRoutine = null;
-            OnAnimationComplete -= _OnAnimationDone;
-            _onAnimationDone?.Invoke();
+            if (!_isHandled)
+            {
+                _isHandled = true;
+                OnAnimationComplete -= _OnAnimationDone;
+                _onAnimationDone?.Invoke();
+            }
         }
 
         /// <summary>
         /// 无论是OnAnimationComplete还是_CoAnimationTimeout(), 都被会调用OnAnimationDone
         /// </summary>
         private Action _onAnimationDone;
-        
-        private CoroutineItem _animationRoutine;
-        
+
+        private bool _isHandled;
+
         /// <summary>
         /// OnAnimationComplete由子类触发, 基类触发的是_CoAnimationTimeout()
         /// </summary>
