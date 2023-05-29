4d8f61a2677c15167d9435bd51bbf6da103a0021
lixianmin <lixianmin@gmail.com>
2023-05-29 16:16:24 +0800

    unicorn项目中不允许出来Client的namespace,     与InputSystem中的某个变量重名了, 会导致编译不过

--- Assets/Code/Client/Web/AssetLoader.cs
+++ Assets/Code/Client/Web/AssetLoader.cs
@@ -1,83 +1,83 @@
-/********************************************************************
-created:    2022-11-14
-author:     lixianmin
-
-Copyright (C) - All Rights Reserved
-*********************************************************************/
-
-using System;
-using System.Collections;
-using Unicorn.Web;
-
-namespace Client.Web
-{
-    public class AssetLoader<T> where T: UnityEngine.Object
-    {
-        private class AssetItem
-        {
-            public IWebNode Node;   // webItem如果被gc了，则asset也会被回收
-            public T Asset;
-        }
-        
-        public bool LoadAsset(string address, Action<T> handler)
-        {
-            if (string.IsNullOrEmpty(address))
-            {
-                return false;
-            }
-
-            if (_assetItems.Contains(address))
-            {
-                return true;
-            }
-
-            _assetItems[address] = null;    // 加一个哨兵
-            var argument = new WebArgument
-            {
-                key = address
-            };
-
-            WebManager.Instance.LoadAsset(argument, node =>
-            {
-                if (node.IsSucceeded)
-                {
-                    if (node.Asset is T asset)
-                    {
-                        _assetItems[address] = new AssetItem{Node = node, Asset = asset};
-                        Console.WriteLine("asset={0} is loaded successfully", address);
-                        handler?.Invoke(asset);
-                        return;
-                    }
-                }
-                
-                _assetItems.Remove(address);
-                Console.Error.WriteLine("failed to load asset={0}", address);
-            });
-
-            return true;
-        }
-
-        public T GetAsset(string address)
-        {
-            if (string.IsNullOrEmpty(address))
-            {
-                return null;
-            }
-
-            var item = _assetItems[address] as AssetItem;
-            return item?.Asset;
-        }
-
-        // public string GetAnimationClipList()
-        // {
-        //     string str = "";
-        //    foreach(DictionaryEntry clip in _loadedClips) 
-        //    {
-        //         str += clip.ToString() + ":";
-        //    }
-        //     return str;
-        // }
-
-        private readonly Hashtable _assetItems = new();
-    }
-}
+// /********************************************************************
+// created:    2022-11-14
+// author:     lixianmin
+//
+// Copyright (C) - All Rights Reserved
+// *********************************************************************/
+//
+// using System;
+// using System.Collections;
+// using Unicorn.Web;
+//
+// namespace Client.Web
+// {
+//     public class AssetLoader<T> where T: UnityEngine.Object
+//     {
+//         private class AssetItem
+//         {
+//             public IWebNode Node;   // webItem如果被gc了，则asset也会被回收
+//             public T Asset;
+//         }
+//         
+//         public bool LoadAsset(string address, Action<T> handler)
+//         {
+//             if (string.IsNullOrEmpty(address))
+//             {
+//                 return false;
+//             }
+//
+//             if (_assetItems.Contains(address))
+//             {
+//                 return true;
+//             }
+//
+//             _assetItems[address] = null;    // 加一个哨兵
+//             var argument = new WebArgument
+//             {
+//                 key = address
+//             };
+//
+//             WebManager.Instance.LoadAsset(argument, node =>
+//             {
+//                 if (node.IsSucceeded)
+//                 {
+//                     if (node.Asset is T asset)
+//                     {
+//                         _assetItems[address] = new AssetItem{Node = node, Asset = asset};
+//                         Console.WriteLine("asset={0} is loaded successfully", address);
+//                         handler?.Invoke(asset);
+//                         return;
+//                     }
+//                 }
+//                 
+//                 _assetItems.Remove(address);
+//                 Console.Error.WriteLine("failed to load asset={0}", address);
+//             });
+//
+//             return true;
+//         }
+//
+//         public T GetAsset(string address)
+//         {
+//             if (string.IsNullOrEmpty(address))
+//             {
+//                 return null;
+//             }
+//
+//             var item = _assetItems[address] as AssetItem;
+//             return item?.Asset;
+//         }
+//
+//         // public string GetAnimationClipList()
+//         // {
+//         //     string str = "";
+//         //    foreach(DictionaryEntry clip in _loadedClips) 
+//         //    {
+//         //         str += clip.ToString() + ":";
+//         //    }
+//         //     return str;
+//         // }
+//
+//         private readonly Hashtable _assetItems = new();
+//     }
+// }
--- Assets/Code/Client/Web/GameWebManager.cs
+++ Assets/Code/Client/Web/GameWebManager.cs
@@ -1,57 +1,57 @@
-/********************************************************************
-created:    2022-08-12
-author:     lixianmin
-
-Copyright (C) - All Rights Reserved
-*********************************************************************/
-using System;
-using Unicorn.Web;
-
-namespace Client.Web
-{
-    public class GameWebManager : WebManager
-    {
-        static GameWebManager()
-        {
-        }
-
-        private GameWebManager()
-        {
-        }
-
-        public IWebNode LoadAsset(string key, Action<IWebNode> handler)
-        {
-            var argument = new WebArgument { key = key };
-            var webItem = new WebItem(argument, handler);
-            return webItem;
-        }
-        
-        public override IWebNode LoadAsset(WebArgument argument, Action<IWebNode> handler)
-        {
-            var webItem = new WebItem(argument, handler);
-            return webItem;
-        }
-        
-        public WebPrefab LoadPrefab(string key, Action<WebPrefab> handler)
-        {
-            var argument = new WebArgument { key = key };
-            var prefab = LoadPrefab(argument, handler);
-            return prefab;
-        }
-        
-        public WebScene LoadScene(string key, Action<WebScene> handler)
-        {
-            var argument = new WebArgument { key = key };
-            var webItem = new WebScene(argument, handler);
-            return webItem;
-        }
-        
-        public WebScene LoadScene(WebArgument argument, Action<WebScene> handler)
-        {
-            var webItem = new WebScene(argument, handler);
-            return webItem;
-        }
-
-        public new static readonly GameWebManager Instance = new();
-    }
-}
+// /********************************************************************
+// created:    2022-08-12
+// author:     lixianmin
+//
+// Copyright (C) - All Rights Reserved
+// *********************************************************************/
+// using System;
+// using Unicorn.Web;
+//
+// namespace Client.Web
+// {
+//     public class GameWebManager : WebManager
+//     {
+//         static GameWebManager()
+//         {
+//         }
+//
+//         private GameWebManager()
+//         {
+//         }
+//
+//         public IWebNode LoadAsset(string key, Action<IWebNode> handler)
+//         {
+//             var argument = new WebArgument { key = key };
+//             var webItem = new WebItem(argument, handler);
+//             return webItem;
+//         }
+//         
+//         public override IWebNode LoadAsset(WebArgument argument, Action<IWebNode> handler)
+//         {
+//             var webItem = new WebItem(argument, handler);
+//             return webItem;
+//         }
+//         
+//         public WebPrefab LoadPrefab(string key, Action<WebPrefab> handler)
+//         {
+//             var argument = new WebArgument { key = key };
+//             var prefab = LoadPrefab(argument, handler);
+//             return prefab;
+//         }
+//         
+//         public WebScene LoadScene(string key, Action<WebScene> handler)
+//         {
+//             var argument = new WebArgument { key = key };
+//             var webItem = new WebScene(argument, handler);
+//             return webItem;
+//         }
+//         
+//         public WebScene LoadScene(WebArgument argument, Action<WebScene> handler)
+//         {
+//             var webItem = new WebScene(argument, handler);
+//             return webItem;
+//         }
+//
+//         public new static readonly GameWebManager Instance = new();
+//     }
+// }
