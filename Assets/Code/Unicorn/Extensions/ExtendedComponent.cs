465b785fa81732be2c11ba061803dbf18b9f7b0e
lixianmin <lixianmin@gmail.com>
2023-10-01 19:51:00 +0800

    给Component补两个AddComponent, SetDefaultCompoent的方法

--- Assets/Code/Unicorn/Extensions/ExtendedComponent.cs
+++ Assets/Code/Unicorn/Extensions/ExtendedComponent.cs
@@ -1,18 +1,65 @@
-﻿
-/********************************************************************
+﻿/********************************************************************
 created:    2014-11-01
 author:     lixianmin
 
 Copyright (C) - All Rights Reserved
 *********************************************************************/
+
 using UnityEngine;
 using System;
 
 namespace Unicorn
 {
-    //[Obfuscators.ObfuscatorIgnore]
     public static class ExtendedComponent
     {
+        public static Component AddComponent(this Component component, Type type)
+        {
+            if (component != null && type != null)
+            {
+                var go = component.gameObject;
+                if (go != null)
+                {
+                    return go.AddComponent(type);
+                }
+            }
+
+            return null;
+        }
+
+        public static T AddComponent<T>(this Component component) where T : Component
+        {
+            if (component != null)
+            {
+                var go = component.gameObject;
+                if (go != null)
+                {
+                    return go.AddComponent(typeof(T)) as T;
+                }
+            }
+
+            return null;
+        }
+
+        public static Component SetDefaultComponent(this Component component, Type type)
+        {
+            if (component != null)
+            {
+                return component.gameObject.SetDefaultComponent(type);
+            }
+
+            return null;
+        }
+
+        public static T SetDefaultComponent<T>(this Component component) where T : Component
+        {
+            if (component != null)
+            {
+                return component.gameObject.SetDefaultComponent<T>();
+            }
+
+            return null;
+        }
+
 //		public static Component GetComponentEx (this Component component, Type type)
 //		{
 //			if (null != component && null != type)
@@ -33,7 +80,7 @@ namespace Unicorn
 //			return null;
 //		}
 
-        public static void SetActive (this Component component, bool isActive)
+        public static void SetActive(this Component component, bool isActive)
         {
             if (null != component)
             {
