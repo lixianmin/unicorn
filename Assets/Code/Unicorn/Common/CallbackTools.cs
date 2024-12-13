999bd3b02a0ea955816f3ef40ee930dbd391f747
lixianmin <lixianmin@live.cn>
2024-12-13 14:10:53 +0800

    CallbackTools.Handle()补两个参数的回调方法

--- Assets/Code/Unicorn/Common/CallbackTools.cs
+++ Assets/Code/Unicorn/Common/CallbackTools.cs
@@ -21,7 +21,7 @@ namespace Unicorn
                 }
                 catch (Exception ex)
                 {
-                    Logo.Warn($"[CallbackTools.Handle()] {title}, ex= {ex},\n\n StackTrace={ex.StackTrace}");
+                    Logo.Warn("[CallbackTools.Handle()] {0}, ex= {1},\n\n StackTrace={2}", title, ex, ex.StackTrace);
                 }
             }
         }
@@ -36,7 +36,7 @@ namespace Unicorn
                 }
                 catch (Exception ex)
                 {
-                    Logo.Warn($"[CallbackTools.Handle()] {title}, ex= {ex},\n\n StackTrace={ex.StackTrace}");
+                    Logo.Warn("[CallbackTools.Handle()] {0}, ex= {1},\n\n StackTrace={2}", title, ex, ex.StackTrace);
                 }
                 finally
                 {
@@ -57,12 +57,32 @@ namespace Unicorn
                 }
                 catch (Exception ex)
                 {
-                    Logo.Warn(
-                        $"[CallbackTools.Handle()] {title}, ex= {ex},\n\n StackTrace={ex.StackTrace},\n\n this= {self}");
+                    Logo.Warn("[CallbackTools.Handle()] {0}, ex= {1},\n\n StackTrace={2}\n\nself={3}", title, ex,
+                        ex.StackTrace, self);
                 }
                 finally
                 {
                     handler = null; // handler是一个回调，它可能间接引用了大量对象，及时置空;
+                }
+            }
+        }
+
+        public static void Handle<T1, T2>(ref Action<T1, T2> handler, T1 self1, T2 self2, string title = "")
+        {
+            if (handler != null)
+            {
+                try
+                {
+                    handler(self1, self2);
+                }
+                catch (Exception ex)
+                {
+                    Logo.Warn("[CallbackTools.Handle()] {0}, ex= {1},\n\n StackTrace={2}\n\nself1={3}, self2={4}", title, ex,
+                        ex.StackTrace, self1, self2);
+                }
+                finally
+                {
+                    handler = null;
                 }
             }
         }
