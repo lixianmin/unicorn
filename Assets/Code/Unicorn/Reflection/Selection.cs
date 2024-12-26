a4275e375406e82e9fab502085212a6b99a8eda6
lixianmin <lixianmin@live.cn>
2024-12-26 12:24:23 +0800

    open Selection.activeTransform

--- Assets/Code/Unicorn/Reflection/Selection.cs
+++ Assets/Code/Unicorn/Reflection/Selection.cs
@@ -1,55 +1,43 @@
-﻿//
-///********************************************************************
-//created:    2015-03-03
-//author:     lixianmin
-//
-//Copyright (C) - All Rights Reserved
-//*********************************************************************/
-//using UnityEngine;
-//using System;
-//
-//namespace Unicorn.Reflection
-//{
-//	public static class Selection
-//	{
-//		private static System.Reflection.PropertyInfo _activeGameObject;
-//
-//		public static GameObject activeGameObject
-//		{
-//			get
-//			{
-//				if (null == _activeGameObject)
-//				{
-//					_activeGameObject = MyType.GetProperty("activeGameObject");
-//				}
-//
-//				return (GameObject)_activeGameObject.GetValue(null, null);
-//			}
-//			set
-//			{
-//				if (null == _activeGameObject)
-//				{
-//					_activeGameObject = MyType.GetProperty("activeGameObject");
-//				}
-//				
-//				_activeGameObject.SetValue(null, value, null);
-//			}
-//		}
-//
-//		private static Type _myType;
-//
-//		public static Type MyType
-//		{
-//			get
-//			{
-//				if (null == _myType)
-//				{
-//					_myType = System.Type.GetType("UnityEditor.Selection,UnityEditor");
-//				}
-//
-//				return _myType;
-//			}
-//		}
-//
-//	}
-//}
+﻿/********************************************************************
+created:    2015-03-03
+author:     lixianmin
+
+Copyright (C) - All Rights Reserved
+*********************************************************************/
+
+using UnityEngine;
+using System;
+
+namespace Unicorn.Reflection
+{
+    public static class Selection
+    {
+        public static GameObject activeGameObject
+        {
+            get => (GameObject)_GetActiveGameObject().GetValue(null, null);
+            set => _GetActiveGameObject().SetValue(null, value, null);
+        }
+
+        private static System.Reflection.PropertyInfo _GetActiveGameObject()
+        {
+            return _activeGameObject ??= MyType.GetProperty("activeGameObject");
+        }
+
+        public static Transform activeTransform
+        {
+            get => (Transform)_GetActiveTransform().GetValue(null, null);
+            set => _GetActiveTransform().SetValue(null, value, null);
+        }
+
+        private static System.Reflection.PropertyInfo _GetActiveTransform()
+        {
+            return _activeTransform ??= MyType.GetProperty("activeTransform");
+        }
+
+        private static System.Reflection.PropertyInfo _activeGameObject;
+        private static System.Reflection.PropertyInfo _activeTransform;
+
+        private static Type _myType;
+        public static Type MyType => _myType ??= Type.GetType("UnityEditor.Selection,UnityEditor");
+    }
+}
