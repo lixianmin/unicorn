2ac3f8dd5a523022e3df9bb096f3a2c747cc50dc
lixianmin <lixianmin@live.cn>
2025-04-29 17:14:42 +0800

    refactor

--- Assets/Standard Assets/Code/Unicorn/Interfaces/IIsLoaded.cs
+++ Assets/Standard Assets/Code/Unicorn/Interfaces/IIsLoaded.cs
@@ -9,6 +9,7 @@ namespace Unicorn
 {
     /// <summary>
     /// 协程相关的代码, 经常需要判断当前的gameObject是否还存在, 以便决定是否继续执行
+    /// IHaveTransform代替不了IIsLoaded, 比如UIWindowBase就是以标记flag来判断IsLoaded()的
     /// </summary>
     public interface IIsLoaded
     {
