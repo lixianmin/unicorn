97dd642d76dafff3717ad721022b3df0e2857dac
lixianmin <lixianmin@live.cn>
2025-04-29 16:49:53 +0800

    IIsLoaded

---
+++ Assets/Standard Assets/Code/Unicorn/Interfaces/IIsLoaded.cs
@@ -0,0 +1,14 @@
+﻿/*********************************************************************
+created:    2025-04-29
+author:     lixianmin
+
+Copyright (C) - All Rights Reserved
+*********************************************************************/
+
+namespace Unicorn
+{
+    public interface IIsLoaded
+    {
+        bool IsLoaded();
+    }
+}
