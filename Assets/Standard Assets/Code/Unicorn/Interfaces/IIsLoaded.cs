55d8d95d0e5e0208386f07800e4a9ca0f3b6c9db
lixianmin <lixianmin@live.cn>
2025-04-29 16:52:16 +0800

    协程相关的代码, 经常需要判断当前的gameObject是否还存在, 以便决定是否继续执行

--- Assets/Standard Assets/Code/Unicorn/Interfaces/IIsLoaded.cs
+++ Assets/Standard Assets/Code/Unicorn/Interfaces/IIsLoaded.cs
@@ -7,6 +7,9 @@ Copyright (C) - All Rights Reserved
 
 namespace Unicorn
 {
+    /// <summary>
+    /// 协程相关的代码, 经常需要判断当前的gameObject是否还存在, 以便决定是否继续执行
+    /// </summary>
     public interface IIsLoaded
     {
         bool IsLoaded();
