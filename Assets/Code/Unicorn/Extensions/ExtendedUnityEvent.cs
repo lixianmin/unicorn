d04bb454b10e5b6da219143b7a991258bb67bbc0
lixianmin <lixianmin@live.cn>
2025-07-22 14:46:34 +0800

    这个方法叫On(), 而不是At(). 一个原因是evt.后面输入O, 第一个就是On(), 而如果输入的是A, 第一个是AddListener()

--- Assets/Code/Unicorn/Extensions/ExtendedUnityEvent.cs
+++ Assets/Code/Unicorn/Extensions/ExtendedUnityEvent.cs
@@ -12,6 +12,12 @@ namespace Unicorn
 {
     public static class ExtendedUnityEvent
     {
+        /// <summary>
+        /// 这个方法叫On(), 而不是At(). 一个原因是evt.后面输入O, 第一个就是On(), 而如果输入的是A, 第一个是AddListener()
+        /// </summary>
+        /// <param name="my"></param>
+        /// <param name="fn"></param>
+        /// <returns></returns>
         public static Action On(this UnityEvent my, UnityAction fn)
         {
             if (my != null && fn != null)
