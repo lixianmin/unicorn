98c09ac71201e9cc9f78aea13e1434ba17e5e7fe
lixianmin <lixianmin@live.cn>
2025-07-22 17:00:55 +0800

    UIRawImage支持LoadImage方法, 支持加载或下载图片

--- Assets/Code/Unicorn/UI/Scripts/UIRawImage.cs
+++ Assets/Code/Unicorn/UI/Scripts/UIRawImage.cs
@@ -1,5 +1,4 @@
-﻿
-/********************************************************************
+﻿/********************************************************************
 created:    2017-07-28
 author:     lixianmin
 
@@ -16,12 +15,109 @@ See the License for the specific language governing permissions and
 limitations under the License.
 *********************************************************************/
 
+using System;
+using Unicorn.Web;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace Unicorn.UI
 {
     public class UIRawImage : RawImage
     {
+        public IWebNode LoadImage(string imagePath)
+        {
+            const string method = nameof(LoadImage);
+            if (string.IsNullOrEmpty(imagePath))
+            {
+                Logo.Warn($"[{method}] imagePath is empty");
+                return EmptyWebNode.It;
+            }
 
+            if (imagePath == _imagePath)
+            {
+                Logo.Info($"[{method}] load the same image, imagePath={imagePath}");
+                return EmptyWebNode.It;
+            }
+
+            return imagePath.StartsWith("Assets") ? _LoadTexture(imagePath) : _DownloadImage(imagePath);
+        }
+
+        private IWebNode _LoadTexture(string imagePath)
+        {
+            const string method = nameof(_LoadTexture);
+
+            var version = ++_version;
+            var argument = new WebArgument { key = imagePath };
+            return WebManager.It.LoadAsset(argument, node =>
+            {
+                if (!_CheckAcceptImage(method, node, imagePath, version))
+                {
+                    return;
+                }
+
+                if (node.Asset is not Texture2D asset)
+                {
+                    Logo.Warn($"[{method}] asset is not Texture2D, imagePath={imagePath}");
+                    return;
+                }
+
+                _web?.Dispose();
+                _web = node as IDisposable;
+                _imagePath = imagePath;
+
+                texture = asset;
+                Logo.Info($"[{method}] load texture success, imagePath={imagePath}");
+            });
+        }
+
+        private HttpTexture _DownloadImage(string imagePath)
+        {
+            const string method = nameof(_DownloadImage);
+            var version = ++_version;
+            return new HttpTexture(imagePath, node =>
+            {
+                if (!_CheckAcceptImage(method, node, imagePath, version))
+                {
+                    return;
+                }
+
+                _web?.Dispose();
+                _web = node;
+                _imagePath = imagePath;
+
+                texture = node.GetTexture();
+                Logo.Info($"[{method}] download image success, imagePath={imagePath}");
+            });
+        }
+
+        private bool _CheckAcceptImage(string method, IWebNode node, string imagePath, int version)
+        {
+            if (node.Status != WebStatus.Succeeded)
+            {
+                Logo.Info($"[{method}] failed to download, imagePath={imagePath}");
+                return false;
+            }
+
+            if (version != _version)
+            {
+                Logo.Info($"[{method}] version has changed, imagePath={imagePath}");
+                return false;
+            }
+
+            if (this == null)
+            {
+                Logo.Info($"[{method}] rawImage has been destroyed, imagePath={imagePath}");
+                return false;
+            }
+
+            return true;
+        }
+
+        // _web和_imagePath需要同步修改
+        private IDisposable _web;
+        private string _imagePath = string.Empty;
+
+        // _version是提前修改的
+        private int _version;
     }
 }
---
+++ Assets/Code/Unicorn/Web/HttpTexture.cs
@@ -0,0 +1,93 @@
+﻿/********************************************************************
+created:    2024-02-22
+author:     lixianmin
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*********************************************************************/
+
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using Object = UnityEngine.Object;
+
+namespace Unicorn.Web
+{
+    public class HttpTexture : Disposable, IWebNode
+    {
+        public HttpTexture(string url, Action<HttpTexture> fn = null)
+        {
+            if (url.IsNullOrEmpty())
+            {
+                throw new ArgumentNullException(nameof(url));
+            }
+
+            _url = url;
+            Status = WebStatus.Loading;
+
+            var request = UnityWebRequestTexture.GetTexture(url);
+            CoroutineManager.It.StartCoroutine(_CoDownload(request, fn));
+
+            AtDisposing += _AtDisposing;
+        }
+
+        private IEnumerator _CoDownload(UnityWebRequest request, Action<HttpTexture> fn)
+        {
+            request.SendWebRequest();
+            while (!request.isDone)
+            {
+                yield return null;
+            }
+
+            Status = request.result == UnityWebRequest.Result.Success ? WebStatus.Succeeded : WebStatus.Failed;
+            if (Status == WebStatus.Succeeded)
+            {
+                _texture = DownloadHandlerTexture.GetContent(request);
+            }
+
+            request.Dispose();
+            CallbackTools.Handle(ref fn, this);
+        }
+
+        private void _AtDisposing()
+        {
+            if (_texture != null)
+            {
+                Object.Destroy(_texture);
+                _texture = null;
+            }
+
+            Status = WebStatus.None;
+        }
+
+        public Texture2D GetTexture()
+        {
+            return _texture;
+        }
+
+        public string GetUrl()
+        {
+            return _url;
+        }
+
+        public void Cancel()
+        {
+        }
+
+        public WebStatus Status { get; private set; }
+        public Object Asset => _texture;
+
+        private Texture2D _texture;
+        private readonly string _url;
+    }
+}
